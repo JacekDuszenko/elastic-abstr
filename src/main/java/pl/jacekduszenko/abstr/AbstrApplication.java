f27c0e0b4ed26d132757957c14ff6f89ece38f1f
Jacek Duszenko <jacktheripper@jaceks-macbook-pro.home>
2020-03-19 12:53:30 +0100

    setup with spring boot initializer

---
+++ src/main/java/pl/jacekduszenko/abstr/AbstrApplication.java
@@ -0,0 +1,13 @@
+package pl.jacekduszenko.abstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jacekduszenko/abstr/AbstrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacekduszenko.abstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
