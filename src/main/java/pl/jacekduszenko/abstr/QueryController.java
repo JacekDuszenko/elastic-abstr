449199b35d9852f500f326036de6626a6628da66
Jacek Duszenko <jacktheripper@jaceks-macbook-pro.home>
2020-03-19 13:11:58 +0100

    add embedded mongo in pom and add service

--- src/main/java/pl/jacekduszenko/abstr/QueryController.java
+++ src/main/java/pl/jacekduszenko/abstr/QueryController.java
@@ -1,17 +1,25 @@
 package pl.jacekduszenko.abstr;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.jacekduszenko.abstr.model.QueryResult;
+import pl.jacekduszenko.abstr.service.QueryService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class QueryController {
 
-    @RequestMapping("/search")
-    ResponseEntity<QueryResult> searchForQuery(@RequestBody String elasticQuery) {
+    private final QueryService queryService;
 
+    @Autowired
+    public QueryController(QueryService queryService) {
+        this.queryService = queryService;
+    }
+
+    @RequestMapping("/search")
+    QueryResult searchForQuery(@RequestBody String elasticQuery) {
+        return queryService.translateQuery(elasticQuery);
     }
 }
